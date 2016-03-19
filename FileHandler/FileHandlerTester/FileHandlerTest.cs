e09cdd9b53b57b5ea56391bc4f34cd7ddd4628aa
alwasa0b <alalwan@gmail.com>
2016-03-18 21:59:06 -0400

    to verbatim

--- FileHandler/FileHandlerTester/FileHandlerTest.cs
+++ FileHandler/FileHandlerTester/FileHandlerTest.cs
@@ -27,8 +27,8 @@ namespace FileHandlerTester
 
         private static DirectoryInfo Initilize(string folder)
         {
-            var dirs = new DirectoryInfo("d:\\folder\\subfolder");
-            var des = new DirectoryInfo($"D:\\TestFileHandler\\{folder}");
+            var dirs = new DirectoryInfo(@"d:\folder\subfolder");
+            var des = new DirectoryInfo($@"D:\TestFileHandler\{folder}");
             
             foreach (var directoryInfo in des.GetDirectories())
             {
@@ -38,7 +38,7 @@ namespace FileHandlerTester
             des.CreateSubdirectory("Source");
             foreach (var fileInfo in dirs.GetFiles())
             {
-                fileInfo.CopyTo($"D:\\TestFileHandler\\{folder}\\Source\\{fileInfo.Name}", true);
+                fileInfo.CopyTo($@"D:\TestFileHandler\{folder}\Source\{fileInfo.Name}", true);
             }
 
             return des;
@@ -49,8 +49,8 @@ namespace FileHandlerTester
         {
            Initilize("FileOverwriter");
 
-            IFileHandler fileOverwriter = new FileOverwriter<Compress>("D:\\TestFileHandler\\FileOverwriter\\Source",
-                new List<string> { "D:\\TestFileHandler\\FileOverwriter\\Dest" }, _pr);
+            IFileHandler fileOverwriter = new FileOverwriter<Compress>(@"D:\TestFileHandler\FileOverwriter\Source",
+                new List<string> { @"D:\TestFileHandler\FileOverwriter\Dest" }, _pr);
 
             fileOverwriter.CopyFiles();
             fileOverwriter.DeleteTemps();
@@ -62,8 +62,8 @@ namespace FileHandlerTester
             var des = Initilize("FileOverwriter");
             des.CreateSubdirectory("Dest");
 
-            IFileHandler fileOverwriter = new FileOverwriter<Compress>("D:\\TestFileHandler\\FileOverwriter\\Source",
-                new List<string> { "D:\\TestFileHandler\\FileOverwriter\\Dest" }, _pr);
+            IFileHandler fileOverwriter = new FileOverwriter<Compress>(@"D:\TestFileHandler\FileOverwriter\Source",
+                new List<string> { @"D:\TestFileHandler\FileOverwriter\Dest" }, _pr);
             fileOverwriter.CopyFiles();
             fileOverwriter.DeleteTemps();
         }
@@ -72,8 +72,8 @@ namespace FileHandlerTester
         public void TestMoveFileOverwrite()
         {
             Initilize("FileOverwriter");
-            IFileHandler fileOverwriter = new FileOverwriter<Compress>("D:\\TestFileHandler\\FileOverwriter\\Source",
-                new List<string> {"D:\\TestFileHandler\\FileOverwriter\\Dest"}, _pr);
+            IFileHandler fileOverwriter = new FileOverwriter<Compress>(@"D:\TestFileHandler\FileOverwriter\Source",
+                new List<string> {@"D:\TestFileHandler\FileOverwriter\Dest"}, _pr);
             fileOverwriter.MoveFiles();
             fileOverwriter.DeleteTemps();
         }
@@ -84,8 +84,8 @@ namespace FileHandlerTester
             var des = Initilize("FileOverwriter");
             des.CreateSubdirectory("Dest");
 
-            IFileHandler fileOverwriter = new FileOverwriter<Compress>("D:\\TestFileHandler\\FileOverwriter\\Source",
-                new List<string> { "D:\\TestFileHandler\\FileOverwriter\\Dest" }, _pr);
+            IFileHandler fileOverwriter = new FileOverwriter<Compress>(@"D:\TestFileHandler\FileOverwriter\Source",
+                new List<string> { @"D:\TestFileHandler\FileOverwriter\Dest" }, _pr);
             fileOverwriter.MoveFiles();
             fileOverwriter.DeleteTemps();
         }
@@ -95,8 +95,8 @@ namespace FileHandlerTester
         {
             Initilize("FileMerger");
 
-            IFileHandler fileOverwriter = new FileMerger<Compress>("D:\\TestFileHandler\\FileMerger\\Source",
-                new List<string> { "D:\\TestFileHandler\\FileMerger\\Source" }, _pr);
+            IFileHandler fileOverwriter = new FileMerger<Compress>(@"D:\TestFileHandler\FileMerger\Source",
+                new List<string> { @"D:\TestFileHandler\FileMerger\Source" }, _pr);
             fileOverwriter.CopyFiles();
             fileOverwriter.DeleteTemps();
         }
@@ -107,16 +107,16 @@ namespace FileHandlerTester
 
             Merger();
 
-            IFileHandler fileOverwriter = new FileMerger<Compress>("D:\\TestFileHandler\\FileMerger\\Source",
-                new List<string> { "D:\\TestFileHandler\\FileMerger\\Dest" }, _pr);
+            IFileHandler fileOverwriter = new FileMerger<Compress>(@"D:\TestFileHandler\FileMerger\Source",
+                new List<string> { @"D:\TestFileHandler\FileMerger\Dest" }, _pr);
             fileOverwriter.CopyFiles();
             fileOverwriter.DeleteTemps();
         }
 
         private static void Merger()
         {
-            var dirs = new DirectoryInfo("d:\\RSNA2013-M7T\\RAD_CT2_Brain");
-            var des = new DirectoryInfo("D:\\TestFileHandler\\FileMerger");
+            var dirs = new DirectoryInfo(@"d:\folder\subfolder");
+            var des = new DirectoryInfo(@"D:\TestFileHandler\FileMerger");
 
             foreach (var directoryInfo in des.GetDirectories())
             {
@@ -128,12 +128,12 @@ namespace FileHandlerTester
 
             foreach (var fileInfo in dirs.GetFiles())
             {
-                fileInfo.CopyTo($"D:\\TestFileHandler\\FileMerger\\Source\\{fileInfo.Name}", true);
+                fileInfo.CopyTo($@"D:\TestFileHandler\FileMerger\Source\{fileInfo.Name}", true);
             }
 
             foreach (var fileInfo in dirs.GetFiles().Where(p => new Random().Next()%2 == 0))
             {
-                fileInfo.CopyTo($"D:\\TestFileHandler\\FileMerger\\Dest\\{fileInfo.Name}", true);
+                fileInfo.CopyTo($@"D:\TestFileHandler\FileMerger\Dest\{fileInfo.Name}", true);
             }
         }
 
@@ -142,8 +142,8 @@ namespace FileHandlerTester
         {
             Initilize("FileMerger");
 
-            IFileHandler fileOverwriter = new FileMerger<Compress>("D:\\TestFileHandler\\FileMerger\\Source",
-                new List<string> { "D:\\TestFileHandler\\FileMerger\\Dest" }, _pr);
+            IFileHandler fileOverwriter = new FileMerger<Compress>(@"D:\TestFileHandler\FileMerger\Source",
+                new List<string> { @"D:\TestFileHandler\FileMerger\Dest" }, _pr);
             fileOverwriter.MoveFiles();
             fileOverwriter.DeleteTemps();
         }
@@ -154,8 +154,8 @@ namespace FileHandlerTester
         {
             Merger();
 
-            IFileHandler fileOverwriter = new FileMerger<Compress>("D:\\TestFileHandler\\FileMerger\\Source",
-                new List<string> { "D:\\TestFileHandler\\FileMerger\\Dest" }, _pr);
+            IFileHandler fileOverwriter = new FileMerger<Compress>(@"D:\TestFileHandler\FileMerger\Source",
+                new List<string> { @"D:\TestFileHandler\FileMerger\Dest" }, _pr);
             fileOverwriter.MoveFiles();
             fileOverwriter.DeleteTemps();
         }
@@ -164,8 +164,8 @@ namespace FileHandlerTester
         public void TestCopyFileSkipper()
         {
             Initilize("FileSkipper");
-            IFileHandler fileOverwriter = new FileSkipper<Compress>("D:\\TestFileHandler\\FileSkipper\\Source",
-                new List<string> { "D:\\TestFileHandler\\FileSkipper\\Dest" }, _pr);
+            IFileHandler fileOverwriter = new FileSkipper<Compress>(@"D:\TestFileHandler\FileSkipper\Source",
+                new List<string> { @"D:\TestFileHandler\FileSkipper\Dest" }, _pr);
             fileOverwriter.CopyFiles();
             fileOverwriter.DeleteTemps();
         }
@@ -173,8 +173,8 @@ namespace FileHandlerTester
         [TestMethod]
         public void TestCopyFileSkipperDup()
         {
-            IFileHandler fileOverwriter = new FileSkipper<Compress>("D:\\TestFileHandler\\FileSkipper\\FileHandlerSource",
-                new List<string> { "D:\\TestFileHandler\\FileSkipper\\FileHandlerDest" }, _pr);
+            IFileHandler fileOverwriter = new FileSkipper<Compress>(@"D:\TestFileHandler\FileSkipper\FileHandlerSource",
+                new List<string> { @"D:\TestFileHandler\FileSkipper\FileHandlerDest" }, _pr);
             fileOverwriter.CopyFiles();
             fileOverwriter.DeleteTemps();
         }
@@ -183,8 +183,8 @@ namespace FileHandlerTester
         public void TestMoveFileSkipper()
         {
             Initilize("FileSkipper");
-            IFileHandler fileOverwriter = new FileSkipper<Compress>("D:\\TestFileHandler\\FileSkipper\\Source",
-                new List<string> { "D:\\TestFileHandler\\FileSkipper\\Dest" }, _pr);
+            IFileHandler fileOverwriter = new FileSkipper<Compress>(@"D:\TestFileHandler\FileSkipper\Source",
+                new List<string> { @"D:\TestFileHandler\FileSkipper\Dest" }, _pr);
             fileOverwriter.MoveFiles();
             fileOverwriter.DeleteTemps();
         }
@@ -192,8 +192,8 @@ namespace FileHandlerTester
         [TestMethod]
         public void TestMoveFileSkipperDup()
         {
-            var dirs = new DirectoryInfo("d:\\RSNA2013-M7T\\RAD_CT2_Brain");
-            var des = new DirectoryInfo($"D:\\TestFileHandler\\FileSkipper");
+            var dirs = new DirectoryInfo(@"d:\folder\subfolder");
+            var des = new DirectoryInfo($@"D:\TestFileHandler\FileSkipper");
 
             foreach (var directoryInfo in des.GetDirectories())
             {
@@ -205,16 +205,16 @@ namespace FileHandlerTester
 
             foreach (var fileInfo in dirs.GetFiles())
             {
-                fileInfo.CopyTo($"D:\\TestFileHandler\\FileSkipper\\Source\\{fileInfo.Name}", true);
+                fileInfo.CopyTo($@"D:\TestFileHandler\FileSkipper\Source\{fileInfo.Name}", true);
             }
 
             foreach (var fileInfo in dirs.GetFiles().Where(p => new Random().Next() % 2 == 0))
             {
-                fileInfo.CopyTo($"D:\\TestFileHandler\\FileSkipper\\Dest\\{fileInfo.Name}", true);
+                fileInfo.CopyTo($@"D:\TestFileHandler\FileSkipper\Dest\{fileInfo.Name}", true);
             }
 
-            IFileHandler fileOverwriter = new FileSkipper<Compress>("D:\\TestFileHandler\\FileSkipper\\Source",
-                new List<string> { "D:\\TestFileHandler\\FileSkipper\\Dest" }, _pr);
+            IFileHandler fileOverwriter = new FileSkipper<Compress>(@"D:\TestFileHandler\FileSkipper\Source",
+                new List<string> { @"D:\TestFileHandler\FileSkipper\Dest" }, _pr);
             fileOverwriter.MoveFiles();
             fileOverwriter.DeleteTemps();
         }
